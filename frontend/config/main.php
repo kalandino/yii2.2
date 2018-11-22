ad67e2b3c815932b8f13faad8a3a5154aca0c579
kalandino <kalandino777@gmail.com>
2018-11-22 14:02:33 +0300

    Работа к уроку №1

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -36,14 +36,15 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                'about' => 'site/about',
+                'contact' => 'site/contact',
+                'login' => 'site/login',
             ],
-        ],
-        */
+        ],  
     ],
     'params' => $params,
 ];
